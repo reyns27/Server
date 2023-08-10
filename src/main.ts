b43e05f9088f5f0c5ca83d4278c43b6aae0e05a8
JReyns <87094232+reyns27@users.noreply.github.com>
2023-08-10 21:06:49 +0000

    ADD PORT

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   app.enableCors();
-  const PORT = process.env.PORT, "0.0.0.0";
-  await app.listen(3000 || PORT);
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT, "0.0.0.0");
 }
 bootstrap();
