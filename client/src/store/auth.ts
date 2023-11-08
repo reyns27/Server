b9bdfe9284a7abdebb60d10908016fad4ec4d75a
Joel Reynoso <reynoso27@gmail.com>
2023-11-08 12:45:46 -0400

    add auth

--- client/src/store/auth.ts
+++ client/src/store/auth.ts
@@ -1,33 +1,33 @@
-import {create} from 'zustand';
-import {persist} from 'zustand/middleware';
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 import { IUser } from '../interface/user';
 
-type State = {
+interface State {
     token: string,
-    profile:IUser | any,
-    isAuth:boolean
+    profile: IUser | any,
+    isAuth: boolean
 };
 
 type Actions = {
-    setToken: (token:string) => void
-    setProfile:(profile:IUser) => void
+    setToken: (token: string) => void
+    setProfile: (profile: IUser) => void
 };
 
 
 export const useAuthStore = create(persist<State & Actions>(
     (set) => ({
-    token:"",
-    isAuth:false,
-    profile:null,
-    setToken: (token:string) => set(() =>({
-        token,
-        isAuth:true
-    })),
-    setProfile:(profile:IUser) => set(() => ({
-        profile
-    }))
-}),{
-    name:'auth'
+        token: "",
+        isAuth: false,
+        profile: null,
+        setToken: (token: string) => set(() => ({
+            token,
+            isAuth: true
+        })),
+        setProfile: (profile: IUser) => set(() => ({
+            profile
+        }))
+    }), {
+    name: 'auth'
 }
 ));
 
